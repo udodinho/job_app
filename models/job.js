a8cbe775b81d9fcdcc21d73c4652f988326e952b
udodinho <udodinho200@gmail.com>
2023-05-19 15:36:35 -0700

    Add jobs schema

---
+++ models/job.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+const JobSchema = new mongoose.Schema({
+    company: {
+        type: String,
+        required: [true, "Please provide a company name"],
+        maxLength: 50,
+    },
+    position: {
+        type: String,
+        required: [true, "Please provide position"],
+        maxLength: 100,
+    },
+    status: {
+        type: String,
+        enum: ["interviewing", "rejected", "pending"],
+        default: "pending",
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: [true, "Please provide user"],
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model("Job", JobSchema)
