7d4237359c9c9aa8841d481e2d3a694d6e252a53
udodinho <udodinho200@gmail.com>
2023-05-19 15:36:09 -0700

    Update not found error

--- errors/index.js
+++ errors/index.js
@@ -1,11 +1,11 @@
 const CustomAPIError = require("./custom-api")
 const BadRequestError = require("./bad-request")
 const UnauthenticatedError = require("./unauthenticated")
-const NotFound = require("./not-found")
+const NotFoundError = require("./not-found")
 
 module.exports = {
     CustomAPIError,
     BadRequestError,
     UnauthenticatedError,
-    NotFound,
+    NotFoundError
 } 
