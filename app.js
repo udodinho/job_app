46f144937dec607971146d97fe66e266d85e3dbe
udodinho <udodinho200@gmail.com>
2023-05-31 16:47:25 -0700

    Add extra security packages

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 require('dotenv').config()
-require("express-async-errors")
-const errorHandlerMiddleware = require("./middleware/error-handler")
-const notFound = require("./middleware/not-found")
-const authRouter = require("./routes/auth")
-const jobsRouter = require("./routes/jobs")
-const express = require("express")
-const app = express()
-const connectDB = require("./db/connectdb")
-const authenticateUser = require("./middleware/auth")
+require("express-async-errors");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFound = require("./middleware/not-found");
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
+const express = require("express");
+const app = express();
+const connectDB = require("./db/connectdb");
+const authenticateUser = require("./middleware/auth");
 
-app.use(express.json())
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+app.set("trust proxy", 1);
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+);
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/jobs", authenticateUser, jobsRouter)
